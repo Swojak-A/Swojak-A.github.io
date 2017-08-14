880cd8268569e0b63c7852db6b0c9bccf7f247f9
SwA <swojak.a@gmail.com>
2017-08-14 12:06:50 +0200

    progress bar commit
    done:
    jquery progress bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// hidden elements - opacity from 0 to 1
+
 $(document).ready(function () {
     $(".hideme").css({ opacity: 0 });
 });
@@ -15,6 +17,37 @@ $(window).scroll(function () {
             $(this).animate({
                 'opacity': '1'
             }, 1000);
+
+         }
+     });
+});
+
+//
+
+var nullWidth = 0;
+$(document).ready(function () {
+    $(".v75").width(nullWidth + '%');
+    $(".v50").width(nullWidth + '%');
+    $(".v25").width(nullWidth + '%');
+});
+
+$(window).scroll(function () {
+    /* Check the location of each desired element */
+    $('#skillset-section').each(function (i) {
+
+        var bottom_of_object = $(this).offset().top + (0.5 * $(this).outerHeight());
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if (bottom_of_window > bottom_of_object) {
+
+                $(".v75").addClass("progress-bar-transistion");
+                $(".v50").addClass("progress-bar-transistion");
+                $(".v25").addClass("progress-bar-transistion");
+
+                $(".v75").width('75%');
+                $(".v50").width('50%');
+                $(".v25").width('25%');
 
          }
      });
